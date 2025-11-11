e96dd21c0a6b635586b106b860d05fa9023c28c1
jimm <jimm@jimm.horse>
2025-11-11 02:31:36 +0000

    wip

--- build_pqxx.sh
+++ build_pqxx.sh
@@ -2,6 +2,6 @@ git submodule update --init --recursive
 (rm -rf ./external/build || true) 2>/dev/null
 cd external/libpqxx
 make clean
-CXXFLAGS='-O3' ./configure --prefix=$(pwd)/../build 
-make -j64
+CXXFLAGS='-O3' ./configure --prefix=$(pwd)/../build
+make -j$(nproc)
 make install
--- profile_worker_cpu.sh
+++ profile_worker_cpu.sh
@@ -1,3 +1,4 @@
+make clean
 CXX="g++"
 CXXFLAGS="-O3 -march=native -mavx2 -mfma -std=c++17 -I/root/cudanagram/external/build/include \
           -Wno-write-strings -Wno-deprecated-declarations -g -fno-omit-frame-pointer"
@@ -11,3 +12,6 @@ $CXX $CXXFLAGS avx.o database.o worker_cpu.o \
       -o worker_cpu_test \
       worker.cpp worker_cpu_test.cpp dictionary.cpp frequency_map.cpp \
       $LDFLAGS
+
+perf record --call-graph=dwarf -F 999 -- ./worker_cpu_test
+#perf report
