2420f596a7130aeab95684346f86d4106ea795e8
jimm <jimm@jimm.horse>
2025-11-19 13:37:28 -0600

    wip

--- cudanagram.cu
+++ cudanagram.cu
@@ -31,16 +31,16 @@ bool worker::delay_sentences = false;
 atomic<bool> worker::terminated {false};
 
 void handler(int sig) {
-//   void *array[10];
-//   size_t size;
+  void *array[10];
+  size_t size;
 
-//   // get void*'s for all entries on the stack
-//   size = backtrace(array, 10);
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 10);
 
-//   // print out all the frames to stderr
-//   fprintf(stderr, "Error: signal %d:\n", sig);
-//   backtrace_symbols_fd(array, size, STDERR_FILENO);
-//   exit(1);
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
 	worker::terminated = true;
 }
 
