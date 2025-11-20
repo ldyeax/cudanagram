8905d4be93fba5b17873326d8e22db64f3029fdc
jimm <jimm@jimm.horse>
2025-11-19 18:49:19 -0600

    wip

---
+++ m2.sh
@@ -0,0 +1,3 @@
+rm -f sqlite/*
+rm -f sentences/*
+./cudanagram --no-gpu --dictionary mementomoriiftheninethlionatethesun.txt --input eentoriifhnehionsn $1
---
+++ m3.sh
@@ -0,0 +1,3 @@
+rm -f sqlite/*
+rm -f sentences/*
+./cudanagram --no-gpu --dictionary mementomoriiftheninethlionatethesun.txt --input eeefhhiinnnoostt $1
